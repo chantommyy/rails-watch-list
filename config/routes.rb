f8c3a62caad738e9a2d81dd6649321106d7d8e4e
Tommy Chan <yinleongch@gmail.com>
2022-11-17 16:50:48 +0000

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
+  # get 'lists/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  # resources :lists do
-  #   resources :bookmarks
-  # end
+  # root "lists#index"
+
+  # get '/lists', to: 'lists#index'
+
+  # get '/lists/new', to: 'lists#new'
+  # post '/lists', to: 'lists#create'
+
+  # get '/lists/:id', to: 'lists#show', as: :list
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
