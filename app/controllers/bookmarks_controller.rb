67dddf1caa6a07d2ec3087a3777e9d7d225d7b49
Tommy Chan <yinleongch@gmail.com>
2022-11-18 09:01:35 +0000

    rails new

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,14 +1,14 @@
 class BookmarksController < ApplicationController
   def new
-    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
     @list = List.find(params[:list_id])
     @bookmark.list = @list
-    if @list.save
+    if @bookmark.save
       redirect_to list_path(@list)
     else
       render :new, status: :unprocessable_entity
@@ -17,6 +17,7 @@ class BookmarksController < ApplicationController
 
   def destroy
     @bookmark = Bookmark.find(params[:id])
+    @list = @bookmark.list
     @bookmark.destroy
     redirect_to list_path(@list), status: :see_other
   end
@@ -24,6 +25,6 @@ class BookmarksController < ApplicationController
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment)
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
   end
 end
